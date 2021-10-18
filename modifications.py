9f8215e18725a50eb72a143c13dbafd39f2d48da
alo0o0o123 <92690141+alo0o0o123@users.noreply.github.com>
2021-10-19 00:34:19 +0300

    Update modifications.py

--- modifications.py
+++ modifications.py
@@ -1,17 +1,10 @@
 from clients import Client
 
 
-class Modifications(Client):
+class Modifications(Client, dict):
     """This class is to create a temporary object that can retrieve account/client data from client_book dictionary
     so it can modify some information using methods from BankAccount, Client classes """
 
-    def __init__(self, account_id, account_balance, account_password, account_type, nation_id, f_name, l_name, mobile):
-        Client.__init__(self)  # Just to remove the warning
-        self.account_id = account_id
-        self.account_balance = account_balance
-        self.account_password = account_password
-        self.account_type = account_type
-        self.national_id = nation_id
-        self.first_name = f_name
-        self.last_name = l_name
-        self.mobile = mobile
+    def __init__(self, **kwargs):
+        dict.__init__(self, **kwargs)
+        self.__dict__ = self
