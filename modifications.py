08ec9c3d09a6ebbf7ea0495d89b635650d18456e
alo0o0o123 <92690141+alo0o0o123@users.noreply.github.com>
2021-10-19 13:58:05 +0300

    Update modifications.py

--- modifications.py
+++ modifications.py
@@ -5,6 +5,12 @@ class Modifications(Client, dict):
     """This class is to create a temporary object that can retrieve account/client data from client_book dictionary
     so it can modify some information using methods from BankAccount, Client classes """
 
-    def __init__(self, **kwargs):
-        dict.__init__(self, **kwargs)
+    # This function get one item from client_book dictionary
+    def __init__(self, **kwargs):  # This is your standard __init__ method.
+        dict.__init__(self, **kwargs)  # The call to dict.__init__(...) is to utilize the super class constructor method
+        # The final line, self.__dict__ = self makes it so the keyword-arguments (kwargs) you pass to the __init__
+        # method can be accessed like attributes,
+        # for example: current_client.account_id, current_client.account_balance in the code in main.py
         self.__dict__ = self
+
+    # If confused,, check function calling_modifications in main.py
